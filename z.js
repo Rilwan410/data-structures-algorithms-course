c482794e768ff049b57f05029d8970b95d46c58a
Rilwan <rilwan410@hotmail.com>
2024-06-29 10:18:02 -0400

    inital commit

---
+++ z.js
@@ -0,0 +1,19 @@
+
+// function addUpTo(n){
+        
+//     let total = 0
+
+//     for (let i = 0; i <= n; i++){
+//         total += i;
+//     }
+
+//     return total;
+// }
+
+
+
+// function addUpTo(n){
+
+//     return n * (n + 1) / 2;
+
+// }
