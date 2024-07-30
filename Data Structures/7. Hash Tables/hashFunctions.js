e61bf5ca7d308a248b27ecc78ee0821c490d3988
Rilwan <rilwan410@hotmail.com>
2024-07-30 00:43:40 -0400

    notes

---
+++ Data Structures/7. Hash Tables/hashFunctions.js
@@ -0,0 +1,7 @@
+/* Hash Functions
+
+    What makes a good hash function?
+        - Fast (constant time)
+        - Doesn't cluster outputs at specific indices, but distributes uniformly
+        - Deterministic (same input yields same output)
+*/      
---
+++ Data Structures/7. Hash Tables/notes.js
@@ -0,0 +1,5 @@
+/*
+- To implement a hash table, we'll be using an array
+- In order to look up values by key, we need a way to convert keys into valid array indices
+- A function that performs this task is called a hash function
+*/
